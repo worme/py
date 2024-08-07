f1c4768bbf804890360a82a88d993cbf4ec3117d
Jason S. <52556746+worme@users.noreply.github.com>
2024-08-06 22:53:17 -0500

    Define countdown()

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
+# Shaking off some cobwebs
 
-
-
-
-
-
+def countdown(num):
+    """This function takes a number as an argument and counts down to zero."""
+    while num >= 0:
+        print(num)
+        num -= 1
+    else:
+        print("BOOM! goes the dynamite")
 
 
 
 def main():
-    print("Yup, this works.")
+    countdown(20)
 
 
 
 
-
-if __name__== "__main__":
+if __name__ == "__main__":
     main()
